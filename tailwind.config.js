3bd8a91146ec5d5ab33ba55d6fec164c77f4c6a0
aditi-manthri <manthripragada.aditi@gmail.com>
2024-10-31 00:35:04 +0530

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'dark-gray': '#2b2b2b', // Dark container color
+        'green-counter': '#00FF7F', // Green counter color
       },
     },
   },
